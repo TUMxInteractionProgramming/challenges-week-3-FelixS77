55180014f1f3dbdb6abb0d85b6315965565a98d1
felix <felixarduus@gmx.de>
2019-03-29 10:12:08 +0100

    challenge 6

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
+//Hello World
 console.log('app is alive');
 
+//Switching Channel
+//Changing Chat appbar title and location
+//Changing attribute selected
 function switchChannel(newChannel){
     console.log('Turning into channel ' + newChannel);
     $('#chat h1 span').html(newChannel);
     $('#chat h1 small strong a').html('upgrading.never.helps');
     $('#chat h1 small strong a').attr('href','http://w3w.co/upgrading.never.helps');
+    $('.star').attr('src','http://ip.lfe.mw.tum.de/sections/star-o.png');
+
+    $('ul .selected').removeClass('selected');
+    $('li:contains('+ newChannel +')').addClass('selected');
+
+}
+
+// Toggle star to filled
+function markStar(){
+    $('.star').attr('src','http://ip.lfe.mw.tum.de/sections/star.png');
+}
+
+//Changing attribute selected
+function selectTab(tabID){
+    console.log('Changing to tab ' + tabID);
+    $('#tab-bar .selected').removeClass('selected');
+    $('#'+tabID).addClass('selected');
 }
